ee0e178cdb196f32f2b32e3d3dcd911cf1243d36
Jim Brazeal <jim.brazeal@kellogg.northwestern.edu>
2023-04-30 18:20:05 +0000

    Generating model

--- kmdb.rb
+++ kmdb.rb
@@ -69,10 +69,13 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+STUDIO.destroy_all
+MOVIE.destory_all
+ACTOR.destory_all
+ROLE.destory_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
